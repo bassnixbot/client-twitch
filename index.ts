006dffd2816e4a7a63a105a6540f351da55b2e99
bassnix <bassnix27@gmail.com>
2024-11-03 20:10:00 +0800

    enhance the join logic

--- index.ts
+++ index.ts
@@ -7,106 +7,139 @@ import { eq } from "drizzle-orm";
 import commands from "./src/commands";
 import tmi from "tmi.js";
 
+const JOIN_LIMIT = 10;
+const JOIN_INTERVAL = 10000; // 10 seconds
+
 const main = async () => {
-  const channelquery = await db
-    .select()
-    .from(dbchannels)
-    .where(eq(dbchannels.status, true));
-  const activecommands = await db
-    .select()
-    .from(dbcommands)
-    .where(eq(dbcommands.status, true));
+    const channelquery = await db
+        .select()
+        .from(dbchannels)
+        .where(eq(dbchannels.status, true));
+    const activecommands = await db
+        .select()
+        .from(dbcommands)
+        .where(eq(dbcommands.status, true));
 
-  const activechannel = channelquery.map((x) => x.channel) as string[];
-  console.log(activechannel);
-  // console.log(activecommands);
-  connecttwitch(activechannel, activecommands);
+    const activechannel = channelquery.map((x) => x.channel) as string[];
+    console.log(activechannel);
+    // console.log(activecommands);
+    connecttwitch(activechannel, activecommands);
 
-  return;
+    return;
 };
 
 const connecttwitch = (channels: string[], commandslist: SelectCommand[]) => {
-  const clientId = Bun.env.CLIENT_ID!;
-  const accessToken = Bun.env.BOT_OAUTH_TOKEN!;
+    const clientId = Bun.env.CLIENT_ID!;
+    const accessToken = Bun.env.BOT_OAUTH_TOKEN!;
 
-  // reason why we use 2 different twitch client here are because
-  // 1. twurple have a new feature like message reply, and tracking parent message id
-  // 2. tmi js can send messages multiple times, so it would be used only to send array/ heavy text response.
+    // reason why we use 2 different twitch client here are because
+    // 1. twurple have a new feature like message reply, and tracking parent message id
+    // 2. tmi js can send messages multiple times, so it would be used only to send array/ heavy text response.
 
-  const tmiClient: tmi.Client = new tmi.Client({
-    options: {
-      debug: true,
-    },
-    connection: {
-      reconnect: true,
-      secure: true,
-    },
-    identity: {
-      username: Bun.env.BOT_USERNAME,
-      password: Bun.env.BOT_OAUTH_TOKEN,
-    },
-    channels: channels,
-  });
-
-  tmiClient.connect();
-
-  const chatAuthProvider = new StaticAuthProvider(clientId, accessToken);
-
-  const chatClient = new ChatClient({
-    authProvider: chatAuthProvider,
-    channels: channels,
-  });
-
-  chatClient.onMessage(
-    async (
-      channel: string,
-      username: string,
-      text: string,
-      msg: ChatMessage,
-    ) => {
-      if (username === "bassnixbot") return;
-
-      text = text.replace(/^@\w+\s*/, "").trim();
-
-      console.log(text);
-
-      let cmd = text.split(" ");
-      if (msg.isReply) {
-        const regex = /@[a-zA-Z0-9._]+/;
-
-        const match = cmd[0].match(regex);
-
-        if (match) {
-          cmd.splice(0);
-        }
-      }
-
-      const cmdSign = "!";
-      if (text[0] !== cmdSign) return;
-
-      if (cmd[0][0] === cmdSign) {
-        if (cmd[0].length === 1)
-          cmd = cmd.filter((command) => command !== cmd[0]);
-        else cmd[0] = cmd[0].replace("!", "");
-      }
-
-      cmd = cmd.filter((text) => text !== "");
-
-      commands(
-        {
-          channel,
-          text: cmd,
-          username,
-          msg,
-          client: chatClient,
-          tmiClient: tmiClient,
+    const tmiClient: tmi.Client = new tmi.Client({
+        options: {
+            debug: true,
         },
-        commandslist,
-      );
-    },
-  );
+        connection: {
+            reconnect: true,
+            secure: true,
+        },
+        identity: {
+            username: Bun.env.BOT_USERNAME,
+            password: Bun.env.BOT_OAUTH_TOKEN,
+        },
+        channels: [],
+    });
 
-  chatClient.connect();
+    tmiClient.connect();
+
+    const chatAuthProvider = new StaticAuthProvider(clientId, accessToken);
+
+    const chatClient = new ChatClient({
+        authProvider: chatAuthProvider,
+        channels: [],
+    });
+
+    // join the channel set interval
+    const intervalId = setInterval(() => {
+        if (channels.length > 0) {
+            joinChannelsBatch(channels, chatClient, tmiClient);
+        } else {
+            clearInterval(intervalId);
+        }
+    }, JOIN_INTERVAL);
+
+    chatClient.onMessage(
+        async (
+            channel: string,
+            username: string,
+            text: string,
+            msg: ChatMessage,
+        ) => {
+            if (username === "bassnixbot") return;
+
+            text = text.replace(/^@\w+\s*/, "").trim();
+
+            console.log(text);
+
+            let cmd = text.split(" ");
+            if (msg.isReply) {
+                const regex = /@[a-zA-Z0-9._]+/;
+
+                const match = cmd[0].match(regex);
+
+                if (match) {
+                    cmd.splice(0);
+                }
+            }
+
+            const cmdSign = "!";
+            if (text[0] !== cmdSign) return;
+
+            if (cmd[0][0] === cmdSign) {
+                if (cmd[0].length === 1)
+                    cmd = cmd.filter((command) => command !== cmd[0]);
+                else cmd[0] = cmd[0].replace("!", "");
+            }
+
+            cmd = cmd.filter((text) => text !== "");
+
+            commands(
+                {
+                    channel,
+                    text: cmd,
+                    username,
+                    msg,
+                    client: chatClient,
+                    tmiClient: tmiClient,
+                },
+                commandslist,
+            );
+        },
+    );
+
+    chatClient.connect();
 };
 
-main();
+// Function to join a batch of channels
+const joinChannelsBatch = async (
+    channels: string[],
+    client: ChatClient,
+    tmiClient: tmi.Client,
+) => {
+    const channelsBatch = channels.splice(0, JOIN_LIMIT);
+    await Promise.all(
+        channelsBatch.map(async (channel) => {
+            try {
+                await client.join(channel);
+                await tmiClient.join(channel);
+
+                console.log(`Successfully joined #${channel}`);
+            } catch (error) {
+                console.error(`Error joining ${channel}:`, error);
+            }
+        }),
+    );
+};
+
+main().catch(console.error);
